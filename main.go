70ac18d3dfc1248cda72902b9813861957676788
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-10-28 16:56:04 -0700

    db is working. still need to work out some kinks

--- main.go
+++ main.go
@@ -1,146 +1,27 @@
 package main
 
 import (
-
-	"net/http"
-	"fmt"
-	"encoding/json"
 	"log"
-	"io/ioutil"
-	"time"
+	"fmt"
 
 	"github.com/tclohm/project-pizza/models"
-	"github.com/gorilla/mux"
+	"github.com/tclohm/project-pizza/handlers"
 
 )
 
-type Message struct {
-	Success bool
-	Msg string
-	Body string
-}
-
-func Up(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Println("api endpoint hit: return 'Up'")
-
-	m := Message{Success: true, Msg: "Up", Body: "-"}
-
-	res, err := json.Marshal(m)
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
-}
-
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	// Parse multipart form, 10 << 20 specifies a maximum
-	// upload of 10MB files
-	// retrive file from posted form-data
-	r.ParseMultipartForm(10 << 20)
-
-	file, handler, err := r.FormFile("file")
-	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		json.NewEncoder(w).Encode("Error reading buffer")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	buff := make([]byte, 512)
-	_, err = file.Read(buff)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	filetype := http.DetectContentType(buff)
-	if filetype != "image/jpeg" && filetype != "image/png" {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	defer file.Close()
-
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
-	fmt.Println("Filename", handler.Header["Content-Disposition"])
-	fmt.Println("Content-Type", handler.Header["Content-Type"])
-
-	// write temp file within our uploads directory that follows
-	tmpFile, err := ioutil.TempFile("uploads", "upload-*.png")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-	defer tmpFile.Close()
-
-	// read all of the contents of our uploaded file into a byte array
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	tmpFile.Write(fileBytes)
-
-	fmt.Println("Successfully Uploaded File\n")
-	fmt.Println("Temporary file", tmpFile.Name())
-
-	m := Message{Success: true, Msg: "Uploaded!", Body: tmpFile.Name()}
-	res, err := json.Marshal(m)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
-}
-
-func getImage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Println(r)
-	// fs := http.FileServer(http.Dir("/uploads"))
-	// http.Handle("/uploads/", http.StripPrefix("/uploads/", fs))
-}
-
-func handleRequests() {
-	router := mux.NewRouter()
-	router.HandleFunc("/api", Up).Methods("GET")
-	router.HandleFunc("/upload", uploadHandler).Methods("POST")
-	router.HandleFunc("/image/{id}", getImage).Methods("GET")
-	server := &http.Server{
-		Handler: router,
-		Addr: "127.0.0.1:8000",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout: 15 * time.Second,
-	}
-	log.Fatal(server.ListenAndServe())
-}
 
 func main() {
-	_, err := models.InitDB()
+	db, err := models.ConnectDB()
 	if err != nil {
 		log.Println(err)
 	}
+
+	db.AutoMigrate(&models.Image{})
+	db.AutoMigrate(&models.Taste{})
+	db.AutoMigrate(&models.Pizza{})
+
+	dbclient := handlers.DBClient{Db: db}
+	
 	fmt.Println("server running on 8000...")
-	handleRequests()
+	handlers.HandleRequests(dbclient)
 }
--- models/models.go
+++ models/models.go
@@ -9,6 +9,8 @@ import (
 
 	_ "github.com/lib/pq"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 // Time string equivalent to Date.now().toISOString in js
@@ -16,14 +18,24 @@ func GenerateISOString() string {
 	return time.Now().UTC().Format("2006-01-02T15:04:05.999Z07:00")
 }
 
-func InitDB() (*sql.DB, error) {
+func ConnectDB() (*gorm.DB, error) {
 	err := godotenv.Load()
-	pg_connection_string := fmt.Sprintf("port=%s host=%s user=%s "+ 
-		"password=%s dbname=%s sslmode=disable",
-		os.Getenv("PSQL_PORT"), os.Getenv("HOSTNAME"), os.Getenv("PSQL_USER"), "", os.Getenv("PSQL_DATABASE"))
+	pg_connection_string := fmt.Sprintf("host=%s user=%s password=%s  "+ 
+		"dbname=%s port=%s sslmode=disable",
+		os.Getenv("HOSTNAME"), os.Getenv("PSQL_USER"), "", os.Getenv("PSQL_DATABASE"), os.Getenv("PSQL_PORT"))
 
+	sqlDB, err := sql.Open("postgres", pg_connection_string)
+	
+	if err != nil {
+		return nil, err
+	}
 
-	db, err := sql.Open("postgres", pg_connection_string)
+	log.Println("connection:", pg_connection_string)
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+
 	if err != nil {
 		return nil, err
 	}
