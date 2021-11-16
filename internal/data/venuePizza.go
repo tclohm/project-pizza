e3f27614ec5269cdbbc632d4c1e6c606099ae974
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-11-16 14:46:08 -0800

    throwing out some folders

--- internal/data/venuePizza.go
+++
@@ -1,7 +0,0 @@
-package data
-
-type VenuePizza struct {
-	ID int64 `json:"id"`
-	VenueId int64 `json:"venue_id"`
-	PizzaId int64 `json:"pizza_id"`
-}
--- internal/data/venues.go
+++
@@ -1,9 +0,0 @@
-package data
-
-type Venue struct {
-	ID int64 `json:"id"`
-	Name string `json:"name"`
-	Lat float64 `json:"lat"`
-	Lon float64 `json:"lon"`
-	Address string `json:"address"`
-}
