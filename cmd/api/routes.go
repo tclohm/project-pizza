7dd53c1bccf289401f8ec224bfa8c87221e475a7
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-11-22 15:55:13 -0800

    still working on query

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,7 @@ func (app *application) routes() *mux.Router {
 	router := mux.NewRouter()
 	sub := router.PathPrefix("/v1").Subrouter()
 	sub.HandleFunc("/healthcheck", app.healthcheckHandler).Methods("GET")
+	sub.HandleFunc("/pizzas", app.listPizzasHandler).Methods("GET")
 	sub.HandleFunc("/pizzas", app.createPizzaHandler).Methods("POST")
 	sub.HandleFunc("/pizzas/{id:[0-9]+}", app.showPizzaHandler).Methods("GET")
 	sub.HandleFunc("/pizzas/{id:[0-9]+}", app.updatePizzaHandler).Methods("PATCH")
