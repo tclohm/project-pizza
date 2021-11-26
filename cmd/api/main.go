d53bfcfe8ab687a2188d17c154b9238b4caebe73
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-11-26 10:10:01 -0800

    logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"time"
@@ -11,6 +10,7 @@ import (
 	"database/sql"
 
 	"github.com/tclohm/project-pizza/internal/data"
+	"github.com/tclohm/project-pizza/internal/jsonlog"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -31,18 +31,18 @@ type config struct {
 
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
 func main() {
 
 	// stdout streams
-	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	err := godotenv.Load(".env")
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 
 	var cfg config
@@ -68,12 +68,12 @@ func main() {
 
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 
 	defer db.Close()
 
-	logger.Printf("database connection pool established")
+	logger.PrintInfo("database connection pool established", nil)
 
 	app := &application{
 		config: cfg,
@@ -84,15 +84,19 @@ func main() {
 	srv := &http.Server{
 		Addr:			fmt.Sprintf(":%d", cfg.port),
 		Handler:		app.routes(),
+		ErrorLog: 		log.New(logger, "", 0),
 		IdleTimeout:	time.Minute,
 		ReadTimeout:	10 * time.Second,
 		WriteTimeout:	30 * time.Second,
 	}
 
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env": cfg.env,
+	})
 
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 
