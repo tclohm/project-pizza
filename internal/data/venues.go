316e83282f1796faaa7ce87dc21fad58f55b5c1a
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-12-02 10:38:03 -0800

    venue CRUD for internal/data

--- internal/data/venues.go
+++ internal/data/venues.go
@@ -14,7 +14,164 @@ import (
 
 type Venue struct {
 	ID int64 `json:"id"`
+	Name string `json:"name"`
 	Lat float64 `json:"lat"`
 	Lon float64 `json:"lon"`
 	Address string `json:"address"`
+}
+
+func ValidateVenue(v *validator.Validator, venue *Venue) {
+	v.Check(venue.Name != "", "name", "must be provided")
+	v.Check(len(venue.Name) < 500, "name", "must not be more than 500 bytes long")
+	v.Check(venue.Address != "", "address", "must be provided")
+	v.Check(venue.Lat != "", "Lat", "must be provided")
+	v.Check(venue.Lon != "", "Lon", "must be provided")
+}
+
+type VenueModel struct {
+	DB *sql.DB
+}
+
+func (vm VenueModel) Insert(venue *Venue) error {
+	query := `
+	INSERT INTO venues (
+		name, 
+		lat,
+		lon,
+		address,
+	)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id
+	`
+	// args slices containing values for the placeholder parameters from the venue struct
+	args := []interface{}{
+		venue.Name, venue.Lat, venue.Lon, venue.Address
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	// passing in the slice and scanning the system generated id
+	return vm.DB.QueryRowContext(ctx, query, args...).Scan(&venue.ID)
+}
+
+func (vm VenueModel) Get(id int64) (*venue, error) {
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `
+		SELECT id, 
+		name, 
+		lat,
+		lon,
+		address
+		FROM venues WHERE id = $1
+	`
+
+	var venue venue
+	// 3-second timeout deadline
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	// release resources associated with context before Get() is returned
+	// memory leak avoided
+	defer cancel()
+
+	err := vm.DB.QueryRowContext(ctx, query, id).Scan(
+		&venue.ID,
+		&venue.Name,
+		&venue.Lat,
+		&venue.Lon,
+		&venue.Address,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &venue, nil
+}
+
+func (vm VenueModel) Update(venue *venue) error {
+	query := `
+		UPDATE venues
+		SET name = $1,
+		lat = $2,
+		lon = $3,
+		address = $4,
+		WHERE id = $5
+		RETURNING id
+	`
+
+	args := []interface{}{
+		venue.Name,
+		venue.Lat,
+		venue.Lon,
+		venue.Address,
+		venue.ID,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+	// query and scan the new value in
+	err := vm.DB.QueryRowContext(ctx, query, args...).Scan(&venue.ID)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (vm VenueModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+		DELETE FROM venues
+		WHERE id = $1`
+
+	result, err := vm.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rows, err := result.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return ErrRecordNotFound
+	}
+	
+	return nil
+}
+
+
+type MockVenueModel struct {}
+
+func (vm MockVenueModel) Insert(venue *Venue) error {
+	return nil
+}
+
+func (vm MockVenueModel) Get(id int64) (*Venue, error) {
+	return nil, nil
+}
+
+func (vm MockVenueModel) Update(venue *Venue) error {
+	return nil
+}
+
+func (vm MockVenueModel) Delete(id int64) error {
+	return nil
 }
